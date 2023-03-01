730aff08f0829b633c5f5004cffd059de407cd23
Carlos Leon <carlos.leon@dox.cl>
2023-03-01 16:45:12 -0500

    add: parametro para definir el negocio en el protocolo del WorkingTimeForBusiness

--- lib/time/working_time_for_business.ex
+++ lib/time/working_time_for_business.ex
@@ -7,58 +7,66 @@ defprotocol Time.WorkingTimeForBusiness do
     @doc"""
     Check if the defined date is a working day
 
-    ### Parameter:
+    ### Parameters:
 
         - date: Timex.DateTime. Date.
+
+        - business: Atom. Business.
 
     ### Return:
 
         - Boolean.
 
     """
-    def is_working_day?(date)
+    def is_working_day?(date, business)
 
     @doc"""
     Check if the defined date is a working hours
 
-    ### Parameter:
+    ### Parameters:
 
         - date: Timex.DateTime. Date.
+
+        - business: Atom. Business.
 
      ### Return:
 
         - Boolean.
 
     """
-    def is_working_hours?(date)
+    def is_working_hours?(date, business)
 
     @doc"""
     Returns the working hours of a day, given the business
 
-    ### Parameter:
+    ### Parameters:
 
         - date: Timex.DateTime. Date.
+
+        - business: Atom. Business.
 
     ### Return:
 
         - {start_time, end_time}, where each element is {hour, minute, second}
 
     """
-    def working_hours(date)
+    def working_hours(date, business)
 
     @doc"""
     Returns the closest business day after the defined date
 
-    ### Parameter:
+    ### Parameters:
 
         - date: Timex.DateTime. Date.
+
+        - business: Atom. Business.
 
     ### Return:
 
         - Timex.DateTime
 
     """
-    def next_working_day(date)
+    def next_working_day(date, business)
 
 
 end
