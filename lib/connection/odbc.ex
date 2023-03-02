da049a86f10679820f2753415debc142ed3818b0
Carlos Leon <carlos.leon@dox.cl>
2023-03-02 03:33:30 -0500

    create: modulo para el trabajo con la libreria :odbc

---
+++ lib/connection/odbc.ex
@@ -0,0 +1,9 @@
+
+defmodule Connection.Odbc do
+    @moduledoc"""
+    Module for working with the ODBC library. It frees you from having to know how Erlang's :odbc library works.
+    """
+
+
+
+end
