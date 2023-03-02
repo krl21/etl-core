bdf684f62069c7f3a90f73b203d2551b98c964ff
Carlos Leon <carlos.leon@dox.cl>
2023-03-01 23:45:45 -0500

    create: modulo para los request por http

---
+++ lib/connection/http.ex
@@ -0,0 +1,69 @@
+
+defmodule Connection.Http do
+    @moduledoc"""
+    Module for working with the HTTP protocol
+    """
+
+
+    @doc"""
+    POST request
+
+    ### Parameters:
+
+        - body: String.
+
+        - url: String.
+
+        - headers: List of String.
+
+        - recv_timeout: Integer.
+
+    ### Return:
+
+        - {:ok, data} | {:error, data}
+
+    """
+    def post(body, url, headers, recv_timeout \\ 15000)
+        when is_binary(body) and is_binary(url) and
+            is_list(headers) and is_integer(recv_timeout) do
+            HTTPoison.post(
+                url,
+                body,
+                headers,
+                recv_timeout: recv_timeout
+            )
+    end
+
+    @doc"""
+    GET request
+
+    ### Parameters:
+
+        - body: Map.
+
+        - url: String.
+
+        - headers: List os String.
+
+        - timeout: Integer.
+
+        - recv_timeout: Integer.
+
+    ### Return:
+
+        - {:ok, data} | {:error, data}
+
+    """
+    def get(url, headers \\ [{"Accept", "application/json"}], timeout \\ 15000, recv_timeout \\ 15000)
+        when is_binary(url) and is_list(headers) and
+            is_integer(timeout) and is_integer(recv_timeout) do
+            HTTPoison.get(
+                url,
+                headers,
+                timeout: timeout,
+                recv_timeout: recv_timeout
+            )
+    end
+
+
+end
