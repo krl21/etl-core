0cf97f0850d86dfe07e962dfe06ac5fd324b2468
Carlos Leon <carlos.leon@dox.cl>
2023-03-02 01:08:20 -0500

    add: parametros username y password en ticket.get

--- lib/connection/ticket.ex
+++ lib/connection/ticket.ex
@@ -4,14 +4,14 @@ defmodule Connection.Ticket do
     Module for working with access tickets
     """
 
-    alias Connection.Http
+    import Connection.Http, only: [get: 2]
 
     @doc"""
     Returns a valid ticket, depending on the user. By default, the generic user is considered.
 
     ### Parameters:
 
-        - url: String. Url to consult.
+        - url: String. Url to consult. It is assumed that the url contains 2 parameters to replace them: <username>, <password>.
 
         - headers: List.
 
@@ -24,10 +24,13 @@ defmodule Connection.Ticket do
         - {Atom, String} | Exception . The atom can take the values: ok or error. The string will be the ticket or the error message.
 
     """
-    def get(url, headers)
-        when is_binary(url) and is_list(headers) do
+    def get(url, headers, username, password)
+        when is_binary(url) and is_list(headers) and
+            is_binary(username) and is_binary(password) do
             url
-            |> Http.get(headers)
+            |> String.replace("<username>", username)
+            |> String.replace("<password>", password)
+            |> get(headers)
             |> case do
                 {:error, error} ->
                     {:error, error}
