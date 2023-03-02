a9456aee7d1639078ac6fef5a153281f568686aa
Carlos Leon <carlos.leon@dox.cl>
2023-03-02 15:16:57 -0500

    add: odbc en fichero mix.exs

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule EtlCore.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :odbc]
     ]
   end
 
