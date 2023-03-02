04b7632a051fd3a573932b18342bb21beed0bd58
Carlos Leon <carlos.leon@dox.cl>
2023-03-02 13:08:06 -0500

    create: genserver para el borrado de los datos

---
+++ lib/genserver/cleaning.ex
@@ -0,0 +1,18 @@
+
+defmodule Genserver.Cleaning do
+    @moduledoc"""
+    Module/genserver oriented to delete the least updated rows in Bigquery. use the `timestamp` field
+    """
+
+    # use GenServer
+    # require Logger
+
+
+
+
+
+
+
+
+
+end
