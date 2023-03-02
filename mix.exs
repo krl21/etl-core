3f4b9d9257f9ad5c560a5f5d22b885899b5f9d0f
Carlos Leon <carlos.leon@dox.cl>
2023-03-02 00:49:34 -0500

    add: definicion en mix.exs, para subir el proyecto en Hex

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule EtlCore.MixProject do
       elixir: "~> 1.14.0-rc.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      package: package(),
+
     ]
   end
 
@@ -18,13 +20,24 @@ defmodule EtlCore.MixProject do
     ]
   end
 
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Carlos Leon"],
+      licenses: ["Apache License 2.0"],
+      links: %{"Bitbucket" => "https://bitbucket.org/teamdox/etl-core/src/master/"},
+      description: "Base functions for ETL process"
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:timex, "~> 3.7.8"},
       {:poison, "~> 5.0"},
-      {:httpoison, "~> 1.8.2", override: true},
+      {:httpoison, "~> 1.8.2"},
       {:decorator, "~> 1.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
 
 
     ]
