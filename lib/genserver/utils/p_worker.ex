a44fc0b234b5f1c3b1f867ce55253c6cfa412eb4
Carlos Leon <carlos.leon@dox.cl>
2023-03-03 11:56:03 -0500

    create: protocolo para el procesamiento de los mensajes por lote

---
+++ lib/genserver/utils/p_worker.ex
@@ -0,0 +1,24 @@
+
+defprotocol Genserver.Utils.PWorker do
+    @moduledoc"""
+    Protocol oriented to the definition of the function that the message lot will take, processed it if necessary and sent it to the corresponding module, for processing
+    """
+
+    @doc"""
+    Sends to process the batch of messages, according to the business from which it comes
+
+    ### Parameters:
+
+        - batch: List of map. Payloads.
+
+        - batch_id: String. Batch identifier.
+
+        - conn_odbc_pid: Process. Process connecting Elixir and ODBC.
+
+        - business: Atom. Business type.
+
+    """
+    def perform(batch, batch_id, pid, business)
+
+
+end
