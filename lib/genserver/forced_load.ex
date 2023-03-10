e7cae821874caa27eba61c1b5a1b830a168548f9
Carlos Leon <carlos.leon@dox.cl>
2023-03-10 14:31:45 -0500

    refactor: Genserver.ForcedLoad

--- lib/genserver/forced_load.ex
+++ lib/genserver/forced_load.ex
@@ -8,21 +8,22 @@ defmodule Genserver.ForcedLoad do
     require Logger
     import Genserver.Utils.PForcedLoad
 
-    def start_link({business, params}) do
-        GenServer.start_link(__MODULE__, {business, params}, name: :"#{__MODULE__}.#{business}")
+    def start_link({business, _params} = info) do
+        GenServer.start_link(__MODULE__, info, name: :"#{__MODULE__}.#{business}")
     end
 
-    def init({business, params}) do
+    def init({business, _params} = info) do
         Logger.info("#{to_string(__MODULE__)}. Initializing. Business: ---#{to_string(business)}---")
         :erlang.send_after(10_000, self(), :update)
-        {:ok, {business, params}}
+        {:ok, info}
     end
 
     def handle_info(:update, {business, params}) do
-        Logger.debug("#{to_string(__MODULE__)}. Applying duplicate/stale row cleanup in ---#{business}---")
+        Logger.debug("#{to_string(__MODULE__)}. Forced load in ---#{business}---")
 
         run(business, params)
 
+        :erlang.send_after(2_937_600_000, self(), :update)
         {:noreply, {business, params}}
     end
 
