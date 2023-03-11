7761de0af24dafff2fbb3e2dbb017f916208d134
Carlos Leon <carlos.leon@dox.cl>
2023-03-11 15:31:28 -0500

    refactor: funciones en Genserver.Utils.PPostgresDb

--- lib/genserver/utils/p_postgres_db.ex
+++ lib/genserver/utils/p_postgres_db.ex
@@ -5,14 +5,28 @@ defprotocol Genserver.Utils.PPostgresDb do
     """
 
     @doc"""
-    Extracts the new rows from a certain table and inserts them into Bigquery
+    Extracts the new rows from a certain table
 
     ### Parameter:
 
         - business: Atom. Business to which the data points.
 
     """
-    @spec load(atom) :: any
-    def load(business)
+    @spec get_data(atom) :: any
+    def get_data(business)
+
+    @doc"""
+    Extracts the new rows from a certain table
+
+    ### Parameter:
+
+        - batch: List of map. Payloads.
+
+        - conn_odbc_pid: Process. Process connecting Elixir and ODBC.
+
+    """
+    @spec load(list, pid) :: any
+    def load(batch, conn_odbc_pid)
+
 
 end
