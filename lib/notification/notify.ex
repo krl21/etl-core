65c64a52ac1dcda6560268956d770b99ad008665
Carlos Leon <carlos.leon@dox.cl>
2025-01-30 11:06:02 -0500

    change: envio de mensajes al slack por pedazos de 3000 caracteres

--- lib/notification/notify.ex
+++ lib/notification/notify.ex
@@ -4,34 +4,56 @@ defmodule Notification.Notify do
     Module responsible for sending notifications to other spaces: Slack, Email, etc.
     """
 
-    @doc"""
-    Send notifications to Slack
+    @doc """
+    Sends a message to Slack, splitting it into chunks of up to 3000 characters.
 
-    ### Parameter:
-
-        - url: String. Url to send the notification.
-
-        - headers: List.
-
-        - env: String.
-
-        - info: String.
+    ### Parameters:
+        - url: String. The Slack webhook URL.
+        - headers: List. The HTTP headers for the request.
+        - env: String. The environment where the message is sent from.
+        - info: String. The message content to be sent.
 
     """
     def notify_slack(url, headers, env, info)
         when is_binary(url) and is_list(headers)
             and is_binary(env) and is_binary(info) do
-            %{"text" =>
-                "<!here> \n" <>
-                "Environment: #{inspect env} \n" <>
-                info <> "\n"
-            }
-            |> Poison.encode!()
-            |> Connection.Http.post(
-                url,
-                headers
-            )
+
+            info
+            |> chunk_text(3000)
+            |> Enum.with_index()
+            |> Enum.each(fn {message, index} ->
+                text = if index == 0 do
+                        "<!here> \n" <>
+                        "Environment: #{inspect env} \n" <>
+                        message
+                    else
+                        message
+                    end
+
+                %{"text" => text}
+                |> Poison.encode!()
+                |> Connection.Http.post(url, headers)
+            end)
     end
+
+    #
+    # Splits a given string into chunks of a specified size
+    #
+    # ### Parameters:
+    #     - text: String. The input text to be split.
+    #     - size: Integer. The maximum number of characters per chunk.
+    #
+    # ## Returns:
+    #   - A list of strings, each containing up to size characters.
+    #
+    defp chunk_text(text, size) do
+        text
+        |> String.graphemes()
+        |> Enum.chunk_every(size)
+        |> Enum.map(&Enum.join/1)
+    end
+
+
 
     @doc"""
     Send notifications to Slack
