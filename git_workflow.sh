b7e85a0fa9ca98b16e0f07ab51b19abb233ca733
Carlos Leon <carlos.leon@dox.cl>
2025-02-21 12:02:57 -0500

    add: fichero para push a los repos

--- git_workflow.sh
+++ git_workflow.sh
@@ -10,25 +10,25 @@ run_command() {
 # Hacer push a la rama actual
 run_command "git push"
 
-# # Cambiar a la rama master
-# run_command "git checkout master"
+# Cambiar a la rama master
+run_command "git checkout master"
 
-# # Fusionar la rama development en master
-# run_command "git merge development"
+# Fusionar la rama development en master
+run_command "git merge development"
 
-# # Hacer push a master
-# run_command "git push"
+# Hacer push a master
+run_command "git push"
 
 # # Obtener la versión mayor actual usando git-semver y hacer push de la etiqueta
 # run_command "git push origin \$(git semver major)"
 
-# # Cambiar de nuevo a la rama development
-# run_command "git checkout development"
+# Cambiar de nuevo a la rama development
+run_command "git checkout development"
 
-# # Hacer push de la rama development al remoto de GitHub
-# run_command "git push github development"
+# Hacer push de la rama development al remoto de GitHub
+run_command "git push github development"
 
-# # Hacer push de la rama master al remoto de GitHub
-# run_command "git push github master"
+# Hacer push de la rama master al remoto de GitHub
+run_command "git push github master"
 
 echo "¡Proceso completado!"
