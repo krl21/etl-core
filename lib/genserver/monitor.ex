eccf9ef08d6a05cc45f9fc1891e94997b119c036
Carlos Leon <carlos.leon@dox.cl>
2025-09-04 10:20:34 -0400

    delete: alertas diarias del monitos

--- lib/genserver/monitor.ex
+++ lib/genserver/monitor.ex
@@ -17,7 +17,7 @@ defmodule Genserver.Monitor do
     def init(state) do
         Logger.info("#{to_string(__MODULE__)}. Initializing.")
 
-        schedule_heartbeat()
+        # schedule_heartbeat()
         {:ok, state}
     end
 
@@ -42,12 +42,12 @@ defmodule Genserver.Monitor do
         {:noreply, %{state | servers: Map.delete(servers, name)}}
     end
 
-    # Send daily status notifications at 8 AM
-    def handle_info(:heartbeat, %{servers: servers, webhook_url: webhook_url, environment: env} = state) do
-        updated_servers = monitor_system(servers, webhook_url, env)
-        schedule_heartbeat()
-        {:noreply, %{state | servers: updated_servers}}
-    end
+    # # Send daily status notifications at 8 AM
+    # def handle_info(:heartbeat, %{servers: servers, webhook_url: webhook_url, environment: env} = state) do
+    #     updated_servers = monitor_system(servers, webhook_url, env)
+    #     schedule_heartbeat()
+    #     {:noreply, %{state | servers: updated_servers}}
+    # end
 
     # Handle crashed processes and notify
     def handle_info({:DOWN, _ref, :process, pid, reason}, %{servers: servers, webhook_url: webhook_url, environment: env} = state) do
@@ -135,21 +135,21 @@ defmodule Genserver.Monitor do
     end
 
 
-    #
-    # Schedules the next heartbeat message to be sent at a randomly generated time.
-    #
-    # ### Return:
-    #    - None. Sends a delayed message to self to trigger the heartbeat.
-    #
-    defp schedule_heartbeat() do
-        now = Timex.now()
-        random_hour = generate_random_hour()
+    # #
+    # # Schedules the next heartbeat message to be sent at a randomly generated time.
+    # #
+    # # ### Return:
+    # #    - None. Sends a delayed message to self to trigger the heartbeat.
+    # #
+    # defp schedule_heartbeat() do
+    #     now = Timex.now()
+    #     random_hour = generate_random_hour()
 
-        next_run = find_next_run_time(now, random_hour)
+    #     next_run = find_next_run_time(now, random_hour)
 
-        delay = Timex.diff(next_run, now, :milliseconds)
-        :erlang.send_after(delay, self(), :heartbeat)
-    end
+    #     delay = Timex.diff(next_run, now, :milliseconds)
+    #     :erlang.send_after(delay, self(), :heartbeat)
+    # end
 
     #
     # Generates a random hour and minute within a specified range.
