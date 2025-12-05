89cbb6e7a6d46afb38efa34e4127ef381751918e
Carlos Leon <carlos.leon@dox.cl>
2025-12-05 06:31:39 -0500

    add: macro para definir atributos y su visibilidad

---
+++ lib/entity/attribute_provider.ex
@@ -0,0 +1,50 @@
+defmodule Entity.AttributeProvider do
+    @moduledoc """
+    Macros and utilities for defining sub-entities that provide attributes.
+
+    ## Usage example:
+
+        defmodule Entity.Record.Buyer do
+            use Entity.AttributeProvider
+
+            @name %Struct.InfoAttr{
+                id: :nombre_comprador,
+                type: :string
+            }
+
+            @rut %Struct.InfoAttr{
+                id: :rut_comprador,
+                id_payload: "comprador_rut",
+                type: :string,
+                keys_to_search: ["data"]
+            }
+
+            attr_list [
+                @name,
+                @rut
+            ]
+
+            # Optional: special processing function
+            def special_post_processing(values, payload) do
+                # Processing logic
+                values
+            end
+        end
+
+    The `attr_list` macro defines the final list that will be returned. Additionally, getter functions are automatically generated for each defined attribute.
+    """
+
+    @doc """
+    Callback invoked when `use Entity.AttributeProvider` is called.
+
+    Imports the necessary macros and sets up the behaviour.
+    """
+    defmacro __using__(_opts) do
+        quote do
+        @behaviour Entity.Behaviour
+        import Entity.AttributeProviderMacro
+        end
+    end
+
+
+end
