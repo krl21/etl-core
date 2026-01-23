4122168bc0b643042edc3de74ba1023e2ebf312c
Carlos Leon <carlos.leon@dox.cl>
2026-01-23 15:33:15 -0500

    fix: eliminar "deps" de la lista de entradas rechazadas en la función de búsqueda

--- lib/stuff.ex
+++ lib/stuff.ex
@@ -193,7 +193,7 @@ defmodule Stuff do
                 {:ok, entries} ->
                     entries
                     |> Enum.reject(fn entry ->
-                        entry in ["_build", "deps", "node_modules"] or
+                        entry in ["node_modules"] or
                         String.starts_with?(entry, ".")
                     end)
                     |> Enum.find_value(fn entry ->
